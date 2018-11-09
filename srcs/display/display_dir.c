4d6290433d785bea781d2fca5f3ac09658901e0d
armand <armandbibiestro@gmail.com>
2018-11-09 19:35:38 +0100

    added begining of a -l

--- srcs/display/display_dir.c
+++ srcs/display/display_dir.c
@@ -6,37 +6,58 @@
 /*   By: abiestro <abiestro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 19:15:42 by abiestro          #+#    #+#             */
-/*   Updated: 2018/11/09 16:49:01 by abiestro         ###   ########.fr       */
+/*   Updated: 2018/11/09 19:16:46 by abiestro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
-
+#include "errno.h"
 #include <sys/types.h>
-
-
 #include <dirent.h>
 
-static void ft_proto_iteration(t_ls_dir *element)
+static void ft_proto_iteration(t_ls *ls, t_ls_dir *element)
 {
     DIR *dir;
     struct dirent *i;
+    t_ls_dir *kid;
+    struct stat info;
+    char buff[200];
 
+    kid = NULL;
+    ft_strcpy(buff, element->name);
     dir = opendir(element->name);
     while (dir && (i = readdir(dir)))
     {
-        ft_printf("%s ", i->d_name);
+        if (ls->display_option[0])
+        {
+            ft_strcpy(buff, element->name);
+            ft_strcat(buff, "/");
+            ft_strcat(buff,i->d_name);
+            if(lstat(buff, &info)!= -1)
+           {
+                kid = ft_new_ls_dir(buff, 0);
+                ft_copy_stat_info_to_ls_dir(kid, &info);
+                ls->display_option[0](kid);
+            }
+            else
+                ft_printf("\n%s\n", strerror(errno));
+        }
+        else
+            ft_printf("%s ", i->d_name);
     }
 }
 
 void        ft_display_dir(t_ls *ls, t_ls_dir *chain)
 {
-     if (ls->display_option[0])
+  /*   if (ls->display_option[0])
         ls->display_option[0](chain);
-    else while (chain)
+    else*/ while (chain)
     {
-        ft_printf("\n\n%s:\n", chain->name);
-        ft_proto_iteration(chain);
+        if (ls->display_option[0])
+            ls->display_option[0](chain);
+        else
+            ft_printf("\n\n%s:\n", chain->name);
+        ft_proto_iteration(ls, chain);
         chain = chain->next;
     }
 }
--- srcs/display/display_l.c
+++ srcs/display/display_l.c
@@ -6,15 +6,79 @@
 /*   By: abiestro <abiestro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 16:25:23 by abiestro          #+#    #+#             */
-/*   Updated: 2018/11/09 16:54:49 by abiestro         ###   ########.fr       */
+/*   Updated: 2018/11/09 18:46:24 by abiestro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+#include "grp.h"
+#include "pwd.h"
+#include "time.h"
+
+static void ft_set_month(char *buff, int month)
+{
+    month++; 
+    if (month == 1)
+        ft_strcpy(buff, "Jan");
+    if (month == 2)
+        ft_strcpy(buff, "Feb");
+    if (month == 3)
+        ft_strcpy(buff, "Mar");
+    if (month == 4)
+        ft_strcpy(buff, "Apr");
+    if (month == 5)
+        ft_strcpy(buff, "May");
+    if (month == 6)
+        ft_strcpy(buff, "Jun");
+    if (month == 7)
+        ft_strcpy(buff, "Jul");
+    if (month == 8)
+        ft_strcpy(buff, "Aug");
+    if (month == 9)
+        ft_strcpy(buff, "Sep");
+    if (month == 10)
+        ft_strcpy(buff, "Oct");
+    if (month == 11)
+        ft_strcpy(buff, "Nov");
+    if (month == 12)
+        ft_strcpy(buff, "Dec");
+}
+
+static char *ft_format_time(struct timespec *time, char *buff)
+{
+    int t;
+    t = time->tv_sec / 31536000 + 1970;
+    ft_itoa(t, buff, 10);
+    buff[4] = ' ';
+    t = time->tv_sec % 31536000 / 2629746;
+    ft_set_month(&buff[5], t);
+    buff[8] = ' ';
+    t = time->tv_sec % 2629746 / 86400 + 1;
+    ft_itoa(t, &buff[9], 10);
+    buff[10] = ' ';
+    t = time->tv_sec % 86400 / 3600 + 1;
+    ft_itoa(t, &buff[11], 10);
+    buff[13] = ':';
+    t = time->tv_sec % 3600 / 60 + 1;
+    ft_itoa(t, &buff[14], 10);
+    return (buff);
+}
 
 void    display_l(t_ls_dir *element)
 {
+    char time[40];
+
+    ft_memset(time, ' ', 39);
+    time[39] = 0;
     if (!element)
     return;
-        ft_printf("idxmod, number, usrer, stuff size date and name %s\n", element->name);
+    ft_format_time(&element->stats.st_mtimespec, time);
+        ft_printf("%0.7d %3d %12s %12s %5u %s %s\n",
+        element->stats.st_mode,
+        element->stats.st_nlink,
+        getpwuid(element->stats.st_uid)->pw_name,
+        getgrgid(element->stats.st_gid)->gr_name,
+        element->stats.st_size,
+        time,
+        element->name);
 }
